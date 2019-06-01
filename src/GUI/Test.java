f02375deaab18795829b7ba7184253532246da2d
9731126 <9731126@ceit.local>
2019-06-02 02:11:17 +0430

    Test class is created to test GUI package

---
+++ src/GUI/Test.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+        // write your code here
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        WindowsGUI mainGUI =new WindowsGUI();
+
+    }
+}
