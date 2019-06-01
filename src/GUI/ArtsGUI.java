96f0314f84e58d58947de1186fd30ca47885bf94
9731126 <9731126@ceit.local>
2019-06-02 01:42:28 +0430

    ArtsGUI class updated

--- src/GUI/ArtsGUI.java
+++ src/GUI/ArtsGUI.java
@@ -1,4 +1,19 @@
 package GUI;
 
-public class ArtsGUI {
-}
+import javax.swing.*;
+import java.awt.*;
+
+public class ArtsGUI extends JPanel{
+    public ArtsGUI() {
+        // super(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        //setBackground(Color.GRAY);
+        JPanel jp = new JPanel( ) ;
+        jp.setLayout( new GridLayout( 1, 4 ) ) ;
+
+        for(int i = 0 ; i < 4 ; i++)
+            for( int j = 0 ; j < 1 ; j ++);
+        //jp.add(new JButton("Button " + j));
+        jp.setVisible(true);
+    }
+}       // setViewportView(jp);
+
