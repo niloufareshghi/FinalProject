a73b8909a4837ed25ca168dbc6bdf0a3de6f7932
9731126 <9731126@ceit.local>
2019-06-02 01:39:44 +0430

    UpSettingGUI has been created!

--- src/GUI/UpSettingGUI.java
+++ src/GUI/UpSettingGUI.java
@@ -1,4 +1,11 @@
 package GUI;
 
-public class UpSettingGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class UpSettingGUI extends JPanel {
+    public UpSettingGUI() {
+        setBackground(Color.CYAN);
+        add(new Button("Demo"));
+    }
 }
