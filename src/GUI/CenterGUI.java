222e14f8b0487ebe8b160781058615d92d7a7d3f
9731126 <9731126@ceit.local>
2019-06-02 01:37:22 +0430

    CenterGUI class Updated!

--- src/GUI/CenterGUI.java
+++ src/GUI/CenterGUI.java
@@ -1,4 +1,15 @@
 package GUI;
 
-public class CenterGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class CenterGUI extends JPanel {
+    UpSettingGUI upSettingGUI= new UpSettingGUI();
+    ArtsGUI artsGUI = new ArtsGUI();
+    public CenterGUI() {
+        setLayout(new BorderLayout());
+        add(upSettingGUI,BorderLayout.PAGE_START);
+        add(artsGUI,BorderLayout.CENTER);
+        //  setSize(600,500);
+    }
 }
