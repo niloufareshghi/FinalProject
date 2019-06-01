7670e49352b706c65263de5391b539bc7416d5fc
9731126 <9731126@ceit.local>
2019-06-02 01:22:29 +0430

    ListGUI class Updated

--- src/GUI/ListGUI.java
+++ src/GUI/ListGUI.java
@@ -1,4 +1,29 @@
 package GUI;
 
-public class ListGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class ListGUI extends JPanel {
+    public ListGUI() {
+        //setSize(w,h);
+        setBackground(Color.DARK_GRAY);
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc =new GridBagConstraints();
+        gbc.fill=GridBagConstraints.BOTH;
+        gbc.weightx=1;
+        gbc.weighty=1;
+        gbc.gridwidth = 1;
+        gbc.gridheight= 1;
+        gbc.gridy = 0;
+        gbc.gridx = 0;
+        JPanel j=new JPanel();
+        j.setBackground(new Color(0xC9000B));
+        add(j,gbc);
+        gbc.gridy =1;
+        gbc.gridx = 0;
+        gbc.weighty=0.5;
+        gbc.gridheight=1;
+        gbc.gridheight= 1;
+        add(new JPanel(),gbc);
+    }
 }
